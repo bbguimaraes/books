58cbfac3c42fa135772c89be26d9cb7ebcbff1f1
Bruno Barcarol Guimarães <bbgstb@gmail.com>
2014-06-26 22:50:19 +0000

    21st_century_c: first chapter

---
+++ 21st_century_c/1/getenv.c
@@ -0,0 +1,11 @@
+#include <stdlib.h> // getev, atoi
+#include <stdio.h> // printf
+
+int main() {
+    char * repstext = getenv("reps");
+    int reps = repstext ? atoi(repstext) : 10;
+    char * msg = getenv("msg");
+    if(!msg) msg = "Hello.";
+    for(int i = 0; i < reps; ++i)
+        printf("%s\n", msg);
+}
